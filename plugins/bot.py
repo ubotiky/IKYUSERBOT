584884d9c3532b413b7c1aa5a857b27d2e3013cc
ubotiky <101111980+ubotiky@users.noreply.github.com>
2022-03-22 13:30:48 +0700

    Update bot.py

--- plugins/bot.py
+++ plugins/bot.py
@@ -192,12 +192,11 @@ async def lol(ult):
 @ultroid_cmd(pattern="ping$", chats=[], type=["official", "assistant"])
 async def _(event):
     start = time.time()
-    x = await event.eor("Pong !")
-await event.eor("sabar ya tod !")
-await event.eor("ahhh.... !")
+    x = await event.eor("Baaa !")
     end = round((time.time() - start) * 1000)
     uptime = time_formatter((time.time() - start_time) * 1000)
-    await x.edit(get_string("🦖ping").format(end, 🦖uptime))
+    await x.edit(get_string("ping").format(end, uptime))
+
 
 
 @ultroid_cmd(
